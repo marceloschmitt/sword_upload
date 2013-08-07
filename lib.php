d33e43caa45d1cf2d155aa001fdaca0c81b0229d
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2013-08-07 15:36:55 -0300

    strstr

--- lib.php
+++ lib.php
@@ -727,7 +727,7 @@ class repository_sword_upload extends repository {
 
         if (empty($saveas_filename)) {
             $filename = str_replace(' ', '', $_FILES['repo_upload_file']['name']);
-	    $filename = strtr(utf8_decode($filename), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
+	    $filename = strstr(utf8_decode($filename), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
         } else {
             $parts =explode(".", $_FILES['repo_upload_file']['name']);
             $extension = end($parts);
